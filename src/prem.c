bb79ebd785de1282586c433aaf603da049217478
Géry <gery.bellanger@gmail.com>
2024-02-27 09:52:21 +0100

    Git créé

---
+++ src/prem.c
@@ -0,0 +1,26 @@
+// #include <cstddef>
+
+/*....................*/
+/* variables globales */
+/*....................*/
+int K,   /* ->gain statique                              */
+    tau, /* ->constante de temps                             */
+    Te;  /* ->période d'échantillonnage                             */
+T;       /* ->période T du signal carré                             */
+durée;   /* ->durée de simulation                             */
+
+/*&&&&&&&&&&&&&&&&&&&&&&*/
+/* aide de ce programme */
+/*&&&&&&&&&&&&&&&&&&&&&&*/
+// void usage( char *pgm_name )
+// {
+//   if( pgm_name == NULL )
+//   {
+//     exit( -1 );
+//   };
+//   printf("%s <hh> <mm> <ss>\n", pgm_name );
+//   printf("declenche un compte a rebours qui va decompte de <hh> <mm> <ss>\n");
+//   printf("jusque 0 en affichant les secondes.\n");
+//   printf("exemple : \n");
+//   printf("%s 12 23 57\n", pgm_name );
+// }
